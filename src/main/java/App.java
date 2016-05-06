996f2cbf62cc0c67a3cd22ebda442b7785301c87
Jeffrey Hackford <Jeffrey.Hackford@gmail.com>
2016-05-06 12:30:14 -0700

    add routes and bug fixes to vtl pages

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,5 +22,90 @@ public class App{
       model.put("template", "templates/add-stylist.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    post("/listStylists", (request, response) ->{
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("stylistInput");
+      Stylist newStylist = new Stylist(name);
+
+      newStylist.save();
+
+      Boolean addedNewStylist = true;
+
+      model.put("stylists", Stylist.all());
+      model.put("addedNewStylist", addedNewStylist);
+      model.put("template", "templates/list-stylists.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/addClient/:id", (request, response) ->{
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      Stylist stylist = Stylist.find(Integer.parseInt(request.params(":id")));
+
+      model.put("stylist", stylist);
+      model.put("template", "templates/add-client.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/addClient", (request,response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String clientName = request.queryParams("clientName");
+      int stylist_id = Integer.parseInt(request.queryParams("stylist_id"));
+
+      Client newClient = new Client(clientName, stylist_id);
+      newClient.save();
+
+      Stylist stylist = Stylist.find(stylist_id);
+
+      Boolean addedNewClient = true;
+
+      model.put("addedNewClient", addedNewClient);
+      model.put("stylist", stylist);
+      model.put("template", "templates/add-client.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/client/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      Client client = Client.find(Integer.parseInt(request.params(":id")));
+      Stylist stylist = Stylist.find(client.getStylistId());
+      model.put("client", client);
+      model.put("stylist", stylist);
+      model.put("template", "templates/client.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/addClient", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/add-client.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/listStylists", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("stylists", Stylist.all());
+      model.put("template", "templates/list-stylists.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/listClients", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("clients", Client.all());
+      model.put("template", "templates/list-clients.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/client/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      Client client = Client.find(Integer.parseInt(request.params(":id")));
+      Stylist stylist = Stylist.find(client.getStylistId());
+
+      model.put("client", client);
+      model.put("stylist", stylist);
+      model.put("template", "templates/client.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -50,7 +50,7 @@ public class Stylist {
     }
   }
   public List<Client> allStylistClients() {
-    String sql = "SELECT id, name, FROM clients WHERE stylist_id=:id";
+    String sql = "SELECT id, name FROM clients WHERE stylist_id=:id";
     try (Connection con = DB.sql2o.open()) {
       return con.createQuery(sql).addParameter("id", id).executeAndFetch(Client.class);
     }
